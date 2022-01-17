7688ea614035121f3fd309d76dd440792bca7d7a
Anik Ronjon <anik35-497@diu.edu.bd>
2022-01-17 23:14:07 +0600

    Style login screen

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -8,59 +8,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool obscureTextValue = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Clash'),
-      ),
-      body: Center(
-        child: Container(
-          child: Column(
-            children: [
-              TextFormField(
-                maxLength: 20,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  labelText: 'Name',
-                  hintText: "Enter Name",
-                  suffixIcon: Icon(
-                    Icons.check_circle,
-                  ),
-                ),
-              ),
-
-              // PassWord Field
-              TextFormField(
-                maxLength: 20,
-                obscureText: obscureTextValue,
-                decoration: InputDecoration(
-                  icon: const Icon(Icons.lock_clock),
-                  labelText: 'Password',
-                  hintText: 'Enter Password',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        obscureTextValue = !obscureTextValue;
-                      });
-                    },
-                    icon: Icon(obscureTextValue
-                        ? Icons.visibility_off
-                        : Icons.visibility),
-                  ),
-                ),
-              ),
-              ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/');
-                  },
-                  icon: const Icon(Icons.lock_open),
-                  label: const Text('Login'))
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('Flutter Clash'),
         ),
-      ),
-    );
+        body: Container(
+          child: const Text('This is Home Screen'),
+        ));
   }
 }
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,13 +1,66 @@
 import 'package:flutter/material.dart';
 
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool obscureTextValue = true;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Login Page"),
+    return Material(
+      child: Container(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/login.png',
+              fit: BoxFit.cover,
+            ),
+            TextFormField(
+              maxLength: 20,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                labelText: 'Name',
+                hintText: "Enter Name",
+                suffixIcon: Icon(
+                  Icons.check_circle,
+                ),
+              ),
+            ),
+
+            // PassWord Field
+            TextFormField(
+              maxLength: 20,
+              obscureText: obscureTextValue,
+              decoration: InputDecoration(
+                icon: const Icon(Icons.lock_clock),
+                labelText: 'Password',
+                hintText: 'Enter Password',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      obscureTextValue = !obscureTextValue;
+                    });
+                  },
+                  icon: Icon(obscureTextValue
+                      ? Icons.visibility_off
+                      : Icons.visibility),
+                ),
+              ),
+            ),
+            ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/');
+                },
+                icon: const Icon(Icons.lock_open),
+                label: const Text('Login'))
+          ],
+        ),
+      ),
     );
   }
 }
